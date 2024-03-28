92ac0926f65fbf250e4a33675b6fb69c8af77d6d
XuanNghia020518 <tranxuannghia020518@gmail.com>
2024-03-28 14:34:34 +0700

    nghia thay doi css lai

--- src/file jsx/DrinksForEossHome/Chitiet.jsx
+++ src/file jsx/DrinksForEossHome/Chitiet.jsx
@@ -90,23 +90,32 @@ function Chitiet(props) {
       <div className="row">
         <div className="col">
           <form action="">
-            <fieldset class="border p-2 fieldset">
-              <legend class="float-none w-auto">
+            <fieldset class="border p-2 fieldset bgsp">
+              <legend class="float-none w-auto chinhchuinfomation">
                 Infomation of {props.Name}
               </legend>
               <pre>
-                <div>Ingredients:</div>
-                {props.Description}
+                <div className="chinhchuchitietsanpham">Ingredients:</div>
+                <div className="chinhnoidungchitietsanpham">
+                  {props.Description}
+                </div>
               </pre>
               <pre>
-                <div>Instructions:</div>
-                {props.Description1}
+                <div className="chinhchuchitietsanpham">Instructions:</div>
+                <div className="chinhnoidungchitietsanpham">
+                  {props.Description1}
+                </div>
               </pre>
               <pre>
-                <div>Instructions:</div>
-                {props.Description2}
+                <div className="chinhchuchitietsanpham">Instructions:</div>
+                <div className="chinhnoidungchitietsanpham">
+                  {props.Description2}
+                </div>
               </pre>
-              <pre><div>Flavor Variations:</div>{props.Description3}</pre>
+              <pre>
+                <div>Flavor Variations:</div>
+                {props.Description3}
+              </pre>
             </fieldset>
           </form>
         </div>
