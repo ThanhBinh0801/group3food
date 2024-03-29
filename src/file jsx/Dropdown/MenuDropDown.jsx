5862ba6fabe4c860bab53bf547f236f8c402fc8c
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-29 09:17:07 +0700

    chinh sua menu

--- src/file jsx/Dropdown/MenuDropDown.jsx
+++ src/file jsx/Dropdown/MenuDropDown.jsx
@@ -3,7 +3,7 @@ import { BsList } from 'react-icons/bs';
 import { useState } from "react";
 import { Offcanvas } from 'react-bootstrap';
 import Popup from "../Contact file/popupcontact";
-import Contact from "../Contact file/Contact";
+
 
 
 function Menu() {
@@ -24,7 +24,7 @@ function Menu() {
         <Offcanvas.Body>
           {/* <hr style={{ borderStyle: 'solid', borderWidth: '4px' }}/> */}
           <p>
-          <Offcanvas.Title style={{ borderBottom: '2px solid black' }}><h3>-Healthy Snack</h3></Offcanvas.Title>
+          <Offcanvas.Title style={{ borderBottom: '2px solid black',textAlign: 'center' }}><h3>Healthy Snack</h3></Offcanvas.Title>
           
             <h5>
               <Link to="/Smooth" className="menuoffcanvas">
@@ -33,18 +33,18 @@ function Menu() {
             </h5>
             <h5><Link to="/Cereal" className="menuoffcanvas">     Cereal</Link></h5>
            
-            <Offcanvas.Title style={{ borderBottom: '2px solid black' }}><h3>- Snacks for kids</h3></Offcanvas.Title>
+            <Offcanvas.Title style={{ borderBottom: '2px solid black' , textAlign: 'center' }}><h3>Snacks for kids</h3></Offcanvas.Title>
             <h5><Link to="/Cakes" className="menuoffcanvas">    Cakes</Link></h5>
             <h5><Link to="/Yogurt" className="menuoffcanvas">    Yogurt</Link></h5>
             
-            <Offcanvas.Title style={{ borderBottom: '2px solid black' }}><h3>- Easy on stomach snacks</h3></Offcanvas.Title>
+            <Offcanvas.Title style={{ borderBottom: '2px solid black' , textAlign: 'center' }}><h3>Easy on stomach snacks</h3></Offcanvas.Title>
             <h5><Link to="/FoodForEOSS" className="menuoffcanvas">     FoodForEOSS</Link></h5>
             <h5><Link to="/DrinksForEoss" className="menuoffcanvas">    DrinkForEOSS</Link></h5>
           </p>
           {/* <hr style={{ borderStyle: 'solid black', borderWidth: '4px' }}/> */}
           <p>          
           <Offcanvas.Header>
-            <Offcanvas.Title><h2>What can we help you?</h2></Offcanvas.Title> 
+            <Offcanvas.Title style={{ borderBottom: '2px solid black' , textAlign: 'center' }}><h2>What can we help you?</h2></Offcanvas.Title> 
           </Offcanvas.Header>        
           <Offcanvas.Title><Link to="/contact" className="menuoffcanvas">Contact</Link></Offcanvas.Title>
           <Popup/>
