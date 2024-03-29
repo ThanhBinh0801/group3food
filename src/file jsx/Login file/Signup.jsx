ddd493b11e6884f3570e1dcc87f4516b34922f8e
ThanhBinh0801 <ninhthanhbinh8197@gmail.com>
2024-03-29 11:46:40 +0700

    chinh sua login

--- src/file jsx/Login file/Signup.jsx
+++ src/file jsx/Login file/Signup.jsx
@@ -15,8 +15,25 @@ function Signup() {
   const [Address, setAddress] = useState("");
   const [Gender, setGender] = useState("");
 
-  const Submitform = (event) => {
-    event.preventDefault();
+  const [isValid, setIsValid] = useState(true);
+
+  const handleChange = (e) => {
+    const input = e.target.value;
+    const regex = /^[0-9]+$/; // Regular expression to match only numbers
+    setIsValid(
+      (input === "" || regex.test(input)) &&
+        input.length >= 9 &&
+        input.length <= 10
+    );
+    setPhone(input);
+  };
+
+  const handleSubmit = () => {
+    if (!isValid) {
+      alert("Please enter a valid phone number with 9 to 10 digits.");
+      return; // Stop further execution if the input is invalid
+    }
+    // Here you can handle form submission
     const newSubmit = {
       Name: Name,
       Password: Password,
@@ -34,7 +51,7 @@ function Signup() {
     console.log("Password:", Password);
     console.log("Phone:", Phone);
     console.log("Address:", Address);
-    console.log("Gender:", Gender)
+    console.log("Gender:", Gender);
     handleClose();
     console.log(newSubmit);
     fetch("https://65d5adc7f6967ba8e3bc3180.mockapi.io/Signup", {
@@ -53,6 +70,11 @@ function Signup() {
       });
   };
 
+  const handleSubmitForm = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    handleSubmit(); // Call the handleSubmit function manually
+  };
+
   return (
     <div className="popup">
       <Button variant="warning" onClick={handleShow}>
@@ -64,7 +86,7 @@ function Signup() {
           <Modal.Title>Sign Up</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={Submitform}>
+          <Form onSubmit={handleSubmitForm}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Your Name</Form.Label>
               <Form.Control
@@ -95,7 +117,8 @@ function Signup() {
               <Form.Control
                 type="text"
                 placeholder="Enter your Phone Number"
-                onChange={(e) => setPhone(e.target.value)}
+                value={Phone}
+                onChange={handleChange}
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -114,7 +137,9 @@ function Signup() {
                 <option value="female">Female</option>
               </Form.Select>
             </Form.Group>
-            <Button type="submit">Send</Button>
+            <Button type="submit">
+              Send
+            </Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
@@ -128,3 +153,4 @@ function Signup() {
 }
 
 export default Signup;
+
